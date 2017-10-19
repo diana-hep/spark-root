de0d8f8158cb9d802f3f555d918127afe9844a81
Pratyush Das <reikdas@gmail.com>
2017-10-19 15:28:51 +0530

    First column test working

--- src/main/scala/org/dianahep/sparkroot/UnitTests/testfirst.scala
+++ src/main/scala/org/dianahep/sparkroot/UnitTests/testfirst.scala
@@ -7,7 +7,29 @@ import org.dianahep.sparkroot._
 
 object testfirst {
 
-  case class generate(myintvector : Array[Int])
+  case class myintvectorclass(myintvector : Array[Integer])
+  case class myvector2class(myvector2 : Array[Array[Integer]])
+  case class vofvofdoubleclass(vofvofdouble : Array[Array[Double]])
+  case class Muonsclass(Muons : Array[Array[Null]])
+  case class aclass(a: Integer)
+  case class bclass(b: Double)
+  case class cclass(c: Float)
+  case class dclass(d: Byte)
+  case class fclass(f: Boolean)
+  case class arr1class(arr1: Array[Integer])
+  case class arr2class(arr2: Array[Double])
+  case class arr3class(arr3: Array[Float])
+  case class arr4class(arr4: Array[Byte])
+  case class arr5class(arr5: Array[Boolean])
+  case class strclass(str: String)
+  case class multi1class(multi1: Array[Array[Array[Integer]]])
+  case class multi2class(multi2: Array[Array[Array[Double]]])
+  case class multi3class(multi3: Array[Array[Array[Float]]])
+  case class multi4class(multi4: Array[Array[Array[Byte]]])
+  case class multi5class(multi5: Array[Array[Array[Boolean]]])
+  case class nclass(n: Integer)
+  case class varr1class(varr1: Array[Integer])
+  case class varr2class(varr2: Array[Double])
 
   def main() {
 
@@ -19,48 +41,48 @@ object testfirst {
 
     import spark.implicits._
 
-    val first = generatedRDD(spark, inputFileName)
-    println(first)
-    val second = createdRDD(spark)
-    println(second)
-    if ((first.except(second)).count()==0 && (second.except(first)).count==0){
+    if (compare(spark,inputFileName)){
       System.out.println("Everything is working correctly")
-    }
-    else {
-      System.out.println("Please check errors")
     }
     spark.stop()
 
   }
 
-  def generatedRDD(spark: SparkSession, inputName: String) : Dataset[Row] = {
+  def compare(spark: SparkSession, inputName: String) : Boolean = {
     val df = spark.sqlContext.read.root(inputName)
-    df.limit(1).select("myintvector")
-  }
+    val flag = true
 
-  def createdRDD(spark: SparkSession) : Dataset[Row] ={
-    /**case class generate(myintvector : Array[Int],myvector2 : Array[Array[Int]],vofvofdouble : Array[Array[Double]],
-                        Muons: Array[Array[Null]],a: Int, b: Double, c: Float, d: Byte, f: Boolean,
-                        arr1 : Array[Int], arr2 : Array[Double],arr3 : Array[Float],arr4: Array[Byte],
-                        arr5: Array[Boolean],str: String, multi1 : Array[Array[Array[Int]]],
-                        multi2 : Array[Array[Array[Double]]], multi3 : Array[Array[Array[Float]]],
-                        multi4 : Array[Array[Array[Byte]]], multi5 : Array[Array[Array[Boolean]]],
-                        n: Int, varr1 : Array[Int], varr2 : Array[Double]) //Add someStruct later
+    def createdmyintvector() : Dataset[Row] = {
+      var c= 0;
+      var finalarr = new Array[Integer](20000)
+      var ds = Seq(myintvectorclass(Array()))
+      for (i <- 0 to 100){
+        for (j <- 0 to i){
+          finalarr(c)=j
+          c=c+1
+        }
+        var arr = new Array[Integer](c)
+        for (j <- 0 to c-1){
+          arr(j)=finalarr(j)
+        }
+        ds=ds :+ myintvectorclass(arr)
+      }
+      import spark.implicits._
+      ds.toDF()
+    }
 
-      val ds = Seq(generate(Array(null),),
-      generate(Array(0),),
-      generate(Array(0,0,1),),
-      generate(Array(0,0,1,0,1.2),),
-      generate(Array(0,0,1,0,1,2,0,1,2,3),),
-      generate(Array(0,0,1,0,1,2,0,1,2,3,0,1,2,3,4),),
-      generate(Array(0,0,1,0,1,2,0,1,2,3,0,1,2,3,4,0,1,2,3,4,5),),
-      generate(Array(0,0,1,0,1,2,0,1,2,3,0,1,2,3,4,0,1,2,3,4,5,0,1,2,3,4,5,6)))
-      **/
+    def comparemyintvector()= {
+      val da = df.select("myintvector")
+      val ds = createdmyintvector()
+      if (da.except(ds).count() != 0 || ds.except(ds).count != 0) {
+        println("myintvector Unit Test failed")
+        System.exit(0)
+      }
+    }
 
 
-    import spark.implicits._
-    val ds = (Seq(generate(Array()))).toDF()
-    ds
+    comparemyintvector
+    flag
   }
 }
 
