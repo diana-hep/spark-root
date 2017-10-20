7f04dfe844459f58978e47013ed618e0ebf14372
Pratyush Das <reikdas@gmail.com>
2017-10-20 11:13:35 +0530

    Added check for third column

--- src/main/scala/org/dianahep/sparkroot/UnitTests/testfirst.scala
+++ src/main/scala/org/dianahep/sparkroot/UnitTests/testfirst.scala
@@ -11,27 +11,27 @@ object testfirst {
 
   case class myintvectorclass(myintvector : Array[Integer])
   case class myvector2class(myvector2 : Array[Array[Integer]])
-  case class vofvofdoubleclass(vofvofdouble : Array[Array[Double]])
+  case class vofvofDoubleclass(vofvofDouble : Array[Array[java.lang.Double]])
   case class Muonsclass(Muons : Array[Array[Null]])
   case class aclass(a: Integer)
-  case class bclass(b: Double)
-  case class cclass(c: Float)
-  case class dclass(d: Byte)
-  case class fclass(f: Boolean)
+  case class bclass(b: java.lang.Double)
+  case class cclass(c: java.lang.Float)
+  case class dclass(d: java.lang.Byte)
+  case class fclass(f: java.lang.Boolean)
   case class arr1class(arr1: Array[Integer])
-  case class arr2class(arr2: Array[Double])
-  case class arr3class(arr3: Array[Float])
-  case class arr4class(arr4: Array[Byte])
-  case class arr5class(arr5: Array[Boolean])
-  case class strclass(str: String)
+  case class arr2class(arr2: Array[java.lang.Double])
+  case class arr3class(arr3: Array[java.lang.Float])
+  case class arr4class(arr4: Array[java.lang.Byte])
+  case class arr5class(arr5: Array[java.lang.Boolean])
+  case class strclass(str: java.lang.String)
   case class multi1class(multi1: Array[Array[Array[Integer]]])
-  case class multi2class(multi2: Array[Array[Array[Double]]])
-  case class multi3class(multi3: Array[Array[Array[Float]]])
-  case class multi4class(multi4: Array[Array[Array[Byte]]])
-  case class multi5class(multi5: Array[Array[Array[Boolean]]])
+  case class multi2class(multi2: Array[Array[Array[java.lang.Double]]])
+  case class multi3class(multi3: Array[Array[Array[java.lang.Float]]])
+  case class multi4class(multi4: Array[Array[Array[java.lang.Byte]]])
+  case class multi5class(multi5: Array[Array[Array[java.lang.Boolean]]])
   case class nclass(n: Integer)
   case class varr1class(varr1: Array[Integer])
-  case class varr2class(varr2: Array[Double])
+  case class varr2class(varr2: Array[java.lang.Double])
 
   def main() {
 
@@ -74,8 +74,6 @@ object testfirst {
     def comparemyintvector()= {
       val da = df.select("myintvector")
       val ds = createdmyintvector()
-      //da.show()
-      //ds.show()
       if (da.except(ds).count() != 0 || ds.except(da).count != 0) {
         println("myintvector Unit Test failed")
         System.exit(0)
@@ -83,7 +81,6 @@ object testfirst {
     }
 
     def createdmyvector2() : Dataset[Row] = {
-
       var ds = Seq[myvector2class]()
       for (i <- 0 to 99){
         var c=0
@@ -116,15 +113,34 @@ object testfirst {
     def comparemyvector2()= {
       val da = df.select("myvector2")
       val ds = createdmyvector2()
-      da.limit(2).show(false)
-      ds.limit(2).show(false)
       if (da.except(ds).count() != 0 || ds.except(da).count != 0) {
         println("myvector2 Unit Test failed")
         System.exit(0)
       }
     }
+
+    def createdvofvofDouble() : Dataset[Row] = {
+      var ds = Seq[vofvofDoubleclass]()
+      var arr = new Array[Array[Double]](1)
+      for (i <- 0 to 99){
+        ds = ds :+ vofvofDoubleclass(Array())
+      }
+      import spark.implicits._
+      ds.toDF()
+    }
+
+    def comparevofvofDouble()= {
+      val da = df.select("vofvofDouble")
+      val ds = createdvofvofDouble()
+      if (da.except(ds).count() != 0 || ds.except(da).count != 0) {
+        println("vofvofDouble Unit Test failed")
+        System.exit(0)
+      }
+    }
+
     comparemyintvector
     comparemyvector2
+    comparevofvofDouble
     flag
   }
 }
